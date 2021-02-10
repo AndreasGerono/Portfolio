1a63f852ca2ef981631bed167d0ca3f0680b5bff
Andreas Gerono <andreas.gerono@rail.bombardier.com>
2021-02-10 20:34:34 +0100

    Add article details and archive view

--- Portfolio/portfolio_projects/urls.py
+++ Portfolio/portfolio_projects/urls.py
@@ -3,4 +3,7 @@ from portfolio_projects import views
 
 urlpatterns = [
     path('', views.index, name="Welcome"),
+    path('<int:year>/', views.year_archive, name='year_archive'),
+    path('<int:year>/<int:month>/', views.month_archive, name='month_archive'),
+    path('<int:year>/<int:month>/<slug:slug>/', views.article_detail, name='article'),
 ]
--- Portfolio/portfolio_projects/views.py
+++ Portfolio/portfolio_projects/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "articles": range(5),
+    }
+    return render(request, 'index.html', context)
+
+
+def article_detail(request, year, month, slug):
+    print(year, month, slug)
+    context = {
+        "article": 'article',
+    }
+    return render(request, 'article.html', context)
+
+
+def month_archive(request, year, month):
+    print(year, month)
+    context = {
+        "articles": range(2),
+    }
+    return render(request, 'archive.html', context)
+
+
+def year_archive(request, year):
+    print(year)
+    context = {
+        "articles": range(10),
+    }
+    return render(request, 'archive.html', context)

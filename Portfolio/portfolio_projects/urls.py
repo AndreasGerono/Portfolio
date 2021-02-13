99199244f6467b44a01d12c1bff4b9683f367bb3
Andreas Gerono <andreas.gerono@rail.bombardier.com>
2021-02-13 23:28:20 +0100

    Add styling for archive and fix url's mistakes

--- Portfolio/portfolio_projects/urls.py
+++ Portfolio/portfolio_projects/urls.py
@@ -4,10 +4,10 @@ from portfolio_projects import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('about', views.about, name="about"),
-    path('article', views.archive, name="article"),
-    path('<int:year>/', views.year_archive, name='year_archive'),
-    path('<int:year>/<int:month>/', views.month_archive, name='month_archive'),
-    path('<int:year>/<int:month>/<slug:slug>/', views.article_detail, name='article'),
+    path('archive', views.archive, name="archive"),
+    path('<int:year>', views.year_archive, name='year_archive'),
+    path('<int:year>/<int:month>', views.month_archive, name='month_archive'),
+    path('<int:year>/<int:month>/<slug:slug>', views.article_detail, name='article'),
 ]
 
 

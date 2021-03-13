ec7b4f3f37d7cb02a3541925e3b1902d02e6cab7
AndreasGerono <andreas.gerono@gmail.com>
2021-03-13 13:16:05 +0100

    Development changes

--- Portfolio/Portfolio/settings/production.py
+++ Portfolio/Portfolio/settings/production.py
@@ -1,9 +1,14 @@
+from decouple import config
 from Portfolio.settings.common import *  # noqa: F403
 
 ALLOWED_HOSTS = ['68.65.122.49', 'localhost']
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']  # noqa: F405
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECRET_KEY = config('SECRET_KEY')  # noqa: F405
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -17,7 +22,7 @@ DATABASES = {
 }
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.  # noqa: E501
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # os.path.join(BASE_DIR, 'staticfiles')  # noqa: E501, F405
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # noqa: E501, F405
 
 # The URL to use when referring to static files (where they will be served from)  # noqa: E501
-STATIC_URL = '/static/'  # noqa: W292
+STATIC_URL = '/static/'  # noqa: W292`
--- Portfolio/manage.py
+++ Portfolio/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings.development')  # noqa: E501
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings.prodcution')  # noqa: E501
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

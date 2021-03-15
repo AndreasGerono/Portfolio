ca91ae4aa472fe32fc8a37daaeb719d16785ec52
AndreasGerono <andreas.gerono@gmail.com>
2021-03-15 18:16:59 +0100

    Add db settings

--- Portfolio/Portfolio/settings/production.py
+++ Portfolio/Portfolio/settings/production.py
@@ -16,8 +16,12 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: F405
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'andrwlkn_portfolio',
+        'USER': 'andrwlkn_admin',
+        'PASSWORD': 'vd6@qiFP!cu{',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
